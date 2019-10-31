8cc52afc28ce5f651a9ec08f1696d2d8936755d3
Nenad Radovanovic <nash.radovanovich@gmail.com>
2019-10-31 10:29:52 +0100

    finished settings page

--- screens/Settings.js
+++ screens/Settings.js
@@ -2,22 +2,49 @@ import React, { Component } from "react";
 import { StyleSheet, Image } from "react-native";
 import Slider from "react-native-slider";
 
-import { Button, Block, Text, Input, Card, Badge, Divider } from "../components";
+import { Button, Block, Text, Switch, Card, Badge, Divider } from "../components";
 import { theme, mocks } from "../constants";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, TextInput } from "react-native-gesture-handler";
 
 class Settings extends Component {
   state = {
     budget: 850,
     cap: 1700,
+    notifications: true,
+    newsletter: false,
+    editing: false,
+    profile: {},
+  };
+  handleEdit = (name, text) => {
+    const { profile } = this.state;
+    profile[name] = text;
+
+    this.setState({ profile });
+  };
+  renderEdit = name => {
+    const { editing, profile } = this.state;
+    if (editing === name) {
+      return <TextInput defaultValue={profile[name]} onChangeText={text => this.handleEdit(name, text)} />;
+    }
+    return <Text bold>{profile[name]}</Text>;
+  };
+  toggleEdit = name => {
+    const { editing } = this.state;
+    this.setState({ editing: !editing ? name : null });
+  };
+  componentDidMount() {
+    this.setState({ profile: this.props.profile });
   }
   render() {
-    const { profile, navigation } = this.props;
+    const { navigation } = this.props;
+    const { profile, editing } = this.state;
 
     return (
       <Block>
         <Block flex={false} row center space="between" style={styles.header}>
-          <Text h1 bold>Settings</Text>
+          <Text h1 bold>
+            Settings
+          </Text>
           <Button onPress={() => navigation.navigate("Settings")}>
             <Image source={profile.avatar} style={styles.avatar} />
           </Button>
@@ -26,33 +53,44 @@ class Settings extends Component {
           <Block style={styles.inputs}>
             <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
               <Block>
-                <Text gray2 style={{ marginBottom: 10 }}>Username</Text>
-                <Text bold>shone</Text>
+                <Text gray2 style={{ marginBottom: 10 }}>
+                  Username
+                </Text>
+                {this.renderEdit("username")}
               </Block>
-              <Text medium secondary>Edit</Text>
+              <Text medium secondary onPress={() => this.toggleEdit("username")}>
+                {editing === "username" ? "Save" : "Edit"}
+              </Text>
             </Block>
             <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
               <Block>
-                <Text gray2 style={{ marginBottom: 10 }}>Location</Text>
-                <Text bold>Belgrade</Text>
+                <Text gray2 style={{ marginBottom: 10 }}>
+                  Location
+                </Text>
+                {this.renderEdit("location")}
               </Block>
-              <Text medium secondary>Edit</Text>
+              <Text medium secondary onPress={() => this.toggleEdit("location")}>
+                {editing === "location" ? "Save" : "Edit"}
+              </Text>
             </Block>
             <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
               <Block>
-                <Text gray2 style={{ marginBottom: 10 }}>E-mail</Text>
-                <Text bold>shone@invoce.com</Text>
+                <Text gray2 style={{ marginBottom: 10 }}>
+                  E-mail
+                </Text>
+                {this.renderEdit("email")}
               </Block>
-              <Text medium secondary>Edit</Text>
             </Block>
           </Block>
 
-          <Divider margin={[10, 0]} />
+          <Divider margin={[theme.sizes.base, theme.sizes.base * 2]} />
 
           <Block style={styles.sliders}>
             <Block margin={[10, 0]}>
-              <Text gray2 style={{ marginBottom: 10 }}>Budget</Text>
-              <Slider 
+              <Text gray2 style={{ marginBottom: 10 }}>
+                Budget
+              </Text>
+              <Slider
                 minimumValue={0}
                 maximumValue={1000}
                 style={{ height: 19 }}
@@ -63,11 +101,15 @@ class Settings extends Component {
                 value={this.state.budget}
                 onValueChange={value => this.setState({ budget: value })}
               />
-              <Text caption gray right>$1.000</Text>
+              <Text caption gray right>
+                $1.000
+              </Text>
             </Block>
             <Block margin={[10, 0]}>
-              <Text gray2 style={{ marginBottom: 10 }}>Monthly Cap</Text>
-              <Slider 
+              <Text gray2 style={{ marginBottom: 10 }}>
+                Monthly Cap
+              </Text>
+              <Slider
                 minimumValue={0}
                 maximumValue={5000}
                 style={{ height: 19 }}
@@ -78,12 +120,26 @@ class Settings extends Component {
                 value={this.state.cap}
                 onValueChange={value => this.setState({ cap: value })}
               />
-              <Text caption gray right>$5.000</Text>
+              <Text caption gray right>
+                $5.000
+              </Text>
             </Block>
+          </Block>
 
           <Divider />
 
-
+          <Block style={styles.toggles}>
+            <Block row center space="between" style={{ marginBottom: theme.sizes.base * 2 }}>
+              <Text gray2>Notifications</Text>
+              <Switch
+                value={this.state.notifications}
+                onValueChange={value => this.setState({ notifications: value })}
+              />
+            </Block>
+            <Block row center space="between" style={{ marginBottom: theme.sizes.base * 2 }}>
+              <Text gray2>Newsletter</Text>
+              <Switch value={this.state.newsletter} onValueChange={value => this.setState({ newsletter: value })} />
+            </Block>
           </Block>
         </ScrollView>
       </Block>
@@ -93,7 +149,7 @@ class Settings extends Component {
 
 Settings.defaultProps = {
   profile: mocks.profile,
-}
+};
 
 export default Settings;
 
@@ -110,7 +166,7 @@ const styles = StyleSheet.create({
     marginTop: theme.sizes.base * 0.7,
   },
   inputRow: {
-    alignItems: "flex-end"
+    alignItems: "flex-end",
   },
   sliders: {
     paddingHorizontal: theme.sizes.base * 2,
@@ -123,5 +179,8 @@ const styles = StyleSheet.create({
     borderColor: "white",
     borderWidth: 3,
     backgroundColor: theme.colors.secondary,
-  }
+  },
+  toggles: {
+    paddingHorizontal: theme.sizes.base * 2,
+  },
 });
